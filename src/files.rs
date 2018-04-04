a97c866a464b7d1007e5154e361d9e34a140ccbf
memoryleak47 <memoryleak47@protonmail.ch>
2018-04-04 02:29:23 +0200

    build basic structure

---
+++ src/files.rs
@@ -0,0 +1,5 @@
+use std::path::PathBuf;
+
+pub fn find_files(path: String) -> Result<Vec<PathBuf>, String> {
+	unimplemented!()
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod files;
+mod words;
+mod typos;
+
+use std::env;
+
+fn main() {
+	let path = env::args().nth(1)
+		.unwrap_or_else(|| panic!("Missing argument"));
+	let files = files::find_files(path)
+		.unwrap_or_else(|x| panic!(x));
+	let words = words::find_words(files);
+	let typos = typos::find_typos(words);
+
+	println!("typo candidates:\n{}", typos);
+}
